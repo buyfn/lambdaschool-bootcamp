c2bc4879e8e7a2cced4cb57f1e475d55ad598046
Ignat <buyfng@gmail.com>
2017-02-11 07:51:44 +0300

    reintroduced jsonify bug

--- assignment_2/homework.py
+++ assignment_2/homework.py
@@ -42,5 +42,5 @@ def substract(minuend, subtrahend):
 @app.route('/favoritefoods')
 def favoritefoods():
     foodlist = ['meat', 'bread', 'hummus']
-    return jsonify(results = foodlist)
+    return jsonify(foodlist)
 
