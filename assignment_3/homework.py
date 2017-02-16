3bb97a1560d739975f5c2130875e4dbda83cc436
Ignat <buyfng@gmail.com>
2017-02-16 17:32:40 +0200

    completed assignment 3

--- assignment_3/homework.py
+++ assignment_3/homework.py
@@ -1,36 +1,74 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+
 @app.route('/enternew')
 def enternew():
     return render_template('food.html')
 
+
 @app.route('/addfood', methods = ['POST'])
 def addfood():
-    connection = sqlite3.connect('assignment_3/database.db')
+    connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
 
-    print(request.form)
-
     try:
-        name = request.form['name']
-        calories = request.form['calories']
-        cuisine = request.form['cuisine']
-        is_vegetarian = request.form['is_vegetarian']
-        is_gluten_free = request.form['is_gluten_free']
-        cursor.execute('INSERT INTO foods (name, calories, cuisine, is_vegetarian, is_gluten_free) VALUES (?, ?, ?, ?, ?)', (name, calories, cuisine, is_vegetarian, is_gluten_free))
+        food = (request.form['name'],
+                request.form['calories'],
+                request.form['cuisine'],
+                request.form['is_vegetarian'],
+                request.form['is_gluten_free'])
+        cursor.execute('INSERT INTO foods VALUES (?, ?, ?, ?, ?)', food)
         connection.commit()
-        message = "added successfully"
+        msg = 'completed with success'
     except:
         connection.rollback()
-        message = "error in insertion"
+        msg = 'failed'
     finally:
         connection.close()
-        return render_template('result.html', message = message)
+        return render_template('result.html', message = msg)
+
+
+@app.route('/favorite')
+def favorite():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    q = 'SELECT * FROM foods WHERE name="bread"'
+    cursor.execute(q)
+    res = cursor.fetchone()
+    connection.close()
+
+    return jsonify(res)
+
+
+@app.route('/search')
+def search():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    searchWord = request.args.get('name')
+    cursor.execute('SELECT * FROM foods WHERE name=?', (searchWord, ))
+    res = cursor.fetchall()
+    connection.close()
+
+    return jsonify(res)
+
+
+@app.route('/drop')
+def drop():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('DROP TABLE foods')
+    connection.close()
+
+    return 'dropped'
 
